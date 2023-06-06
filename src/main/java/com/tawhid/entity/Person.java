3d31a43433d2024debff24132d5b1f27a84b8df6
Md.Tawhidul islam <islamtawhidul910@gmmail.com>
2023-06-07 02:12:43 +0600

    configation application.propertices

--- src/main/java/com/tawhid/entity/Person.java
+++ src/main/java/com/tawhid/entity/Person.java
@@ -4,9 +4,11 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
+@Table(name = "person")
 public class Person {
 
     @Id
